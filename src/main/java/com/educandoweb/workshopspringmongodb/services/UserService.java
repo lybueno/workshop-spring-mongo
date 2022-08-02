a1a44116f15c9d28ec79c0347ebd7d5e633a1dd2
Lygia Bueno <lybueno@gmail.com>
2022-08-02 15:36:36 -0300

    insercao de ususario com POST

--- src/main/java/com/educandoweb/workshopspringmongodb/services/UserService.java
+++ src/main/java/com/educandoweb/workshopspringmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.educandoweb.workshopspringmongodb.services;
 
 import com.educandoweb.workshopspringmongodb.domain.User;
+import com.educandoweb.workshopspringmongodb.dto.UserDTO;
 import com.educandoweb.workshopspringmongodb.repositories.UserRepository;
 import com.educandoweb.workshopspringmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
