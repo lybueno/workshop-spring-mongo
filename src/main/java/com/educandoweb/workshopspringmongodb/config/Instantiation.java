c0044e28c5aa3ea4d4961e1406b7352f4002dee9
Lygia Bueno <lybueno@gmail.com>
2022-08-02 17:15:10 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/educandoweb/workshopspringmongodb/config/Instantiation.java
+++ src/main/java/com/educandoweb/workshopspringmongodb/config/Instantiation.java
@@ -2,6 +2,8 @@ package com.educandoweb.workshopspringmongodb.config;
 
 import com.educandoweb.workshopspringmongodb.domain.Post;
 import com.educandoweb.workshopspringmongodb.domain.User;
+import com.educandoweb.workshopspringmongodb.dto.AuthorDTO;
+import com.educandoweb.workshopspringmongodb.dto.UserDTO;
 import com.educandoweb.workshopspringmongodb.repositories.PostRepository;
 import com.educandoweb.workshopspringmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +36,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-                maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+                new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/educandoweb/workshopspringmongodb/domain/Post.java
+++ src/main/java/com/educandoweb/workshopspringmongodb/domain/Post.java
@@ -1,5 +1,7 @@
 package com.educandoweb.workshopspringmongodb.domain;
 
+import com.educandoweb.workshopspringmongodb.dto.AuthorDTO;
+import com.educandoweb.workshopspringmongodb.dto.UserDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -14,12 +16,12 @@ public class Post {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -59,11 +61,11 @@ public class Post {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 }
