798a2da731c9dff3813249263166620d8174c50d
Lygia Bueno <lybueno@gmail.com>
2022-08-02 13:42:35 -0300

    Projeto criado

---
+++ src/test/java/com/educandoweb/workshopspringmongodb/WorkshopSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshopspringmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
